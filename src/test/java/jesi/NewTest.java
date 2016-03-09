9d87a877d75ac22e01074cd14be8179477e5251b
Srinath Shil <srinath.shil123@gmail.com>
2016-03-09 09:12:08 +0530

    Add url

--- src/test/java/jesi/NewTest.java
+++ src/test/java/jesi/NewTest.java
@@ -1,10 +1,19 @@
 package jesi;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import org.testng.annotations.Test;
 
 public class NewTest {
   @Test
-  public void f() {
+  public void f() throws IOException {
 	  System.out.println("hello1ss");
-  }
+	  URL url= new URL("http://localhost:8089/rest");
+HttpURLConnection con=  (HttpURLConnection) url.openConnection();
+con.setRequestMethod("POST");
+System.out.println("END ofconnection");
+
+}
 }
